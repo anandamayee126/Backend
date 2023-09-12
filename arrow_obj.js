97a520fa375eff872392bc9afb192eed6d253bf4
Anandamayee Ghosh <ghosh.anandamayee>
2023-09-12 09:36:41 +0530

    Fat arrow function and student object done

---
+++ arrow_obj.js
@@ -0,0 +1,13 @@
+//FAT ARROW FUNCTION
+
+const product=(a,b)=> a*b;
+console.log(product(3,4));
+
+//Object
+
+const student={
+    name: 'Anandamayee Ghosh',
+    department: 'Computer Science'
+}
+console.log(student);
+
