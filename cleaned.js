6af37d0c0d4ac0edcced9312404e6780b3999eb3
Anandamayee Ghosh <ghosh.anandamayee>
2023-09-15 22:04:28 +0530

    cleaned

---
+++ cleaned.js
@@ -0,0 +1,43 @@
+const fs= require('fs');
+
+const cleaned= (req,res) => {
+    let url=req.url;
+    const method= req.method;
+    if(url==='/'){
+        //res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>My Server</title></head>');
+        res.write('<body><form action="/messages" method="post"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/messages' && method==='POST')
+    {
+        const body=[];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+       return req.on('end',() =>{
+            const parseBody=Buffer.concat(body).toString();//in the format-> message="something"
+            const message=parseBody.split('=')[1];//part after message
+            console.log(message);
+            fs.writeFile('message.txt',message,err=>{
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        });
+    }
+        //res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Node.js page</title></head>');
+        res.write('<body><h1>Welcome To Node.js page</h1></body>');
+        res.write('</html>');
+        //res.send();
+        res.end();
+}
+
+module.exports={
+    handler: cleaned,
+    someText:"hard coded text"
+};
