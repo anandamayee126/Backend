42b61408f970ec5b892f45b25ff4e2e7c3f338ee
Anandamayee Ghosh <ghosh.anandamayee>
2023-09-19 21:50:18 +0530

    MIDDLEWARE EXECUTION

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app= express();
-const port=3000;
-app.use((req, res, next) =>{
-    console.log('1st middleware');
-    next();
+const port=4000;
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/add-product',(req, res, next) =>{
+    res.send(`<form action="/product" method="POST"><input type=text name="product" value="product-name"><input type="number name="size" value="size"><button type="submit">Add Product</button></form>`);
 })
 
-app.use((req, res, next) =>{
-    console.log('2nd middleware');
-    res.send('<h1>Hello from Express</h1>')
+app.use('/product',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
 })
+app.use('/',(req, res, next) =>{
+    res.send(`<h1>HELLO FROM ANDY</h1>`);
+})
+
+// app.use((req, res, next) =>{
+//     console.log('2nd middleware');
+//     res.send('<h1>Hello from Express</h1>')
+// })
 
 app.listen(port,() =>{
   console.log(`listening on port ${port}`);
